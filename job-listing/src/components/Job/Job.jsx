27051cb9ed839a55394ef6f8adfdb77581a53a53
nayera540 <1900205@eng.asu.edu.eg>
2024-10-10 20:28:27 +0300

    Add responsive design

--- job-listing/src/components/Job/Job.jsx
+++ job-listing/src/components/Job/Job.jsx
@@ -2,10 +2,10 @@ import "./Job.css";
 
 function Job({ job }) {
     return (
-        <div className={`bg-white container flex flex-col md:flex-row mb-6 md:max-w-[1000px] max-w-[500px] shadow-shadow shadow-lg p-6 px-8 rounded-md ${job.featured && "border-l-[5px] border-l-Desaturated-Dark-Cyan"}`}>
-            <img src={job.logo} alt={`${job.company} logo`} className="max-w-[88px] max-h-[88px] mt-1" />
-            <div className="flex md:justify-between md:flex-row flex-col flex-grow">
-                <div className="details flex flex-col mt-2 ml-4">
+        <div className={`bg-white container flex flex-col md:flex-row md:mb-6 mb-10 md:max-w-[1000px] max-w-[500px] shadow-shadow shadow-lg p-6 px-8 rounded-md ${job.featured && "border-l-[5px] border-l-Desaturated-Dark-Cyan"}`}>
+            <img src={job.logo} alt={`${job.company} logo`} className="md:max-w-[88px] md:max-h-[88px] mt-1 max-h-[60px] max-w-[60px] md:top-0 md:left-0 relative -top-12 -left-3" />
+            <div className="flex md:justify-between md:flex-row flex-col flex-grow -ml-3 -mt-9 md:ml-0 md:mt-0">
+                <div className="details flex flex-col md:mt-2 md:ml-4 mb-5 border-b border-b-Dark-Grayish-Cyan border-solid pb-4 md:pb-0 md:border-0 md:mb-0">
                     <div className="flex flex-row mb-1 gap-2">
                         <p className="mr-2 text-Desaturated-Dark-Cyan font-bold">{job.company}</p>
                         {job.new && <span className="uppercase bg-Desaturated-Dark-Cyan font-semibold pt-1 text-sm text-white rounded-2xl h-6 w-12 text-center">new!</span>}
